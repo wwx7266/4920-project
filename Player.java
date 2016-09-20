f10ede408b84a1954bdf305872feeb4a153384dc
wwx7266 <scottwu7266@gmail.com>
2016-09-20 20:34:41 +1000

    version 1.0
    Basic single Client with part function of SandBox Mode

---
+++ Player.java
@@ -0,0 +1,9 @@
+package client;
+
+import java.util.ArrayList;
+
+public class Player {
+	private String UserName;
+	private ArrayList<Unit> Army;
+	private ArrayList<Spot> domain;
+}
---
+++ Spot.java
@@ -0,0 +1,11 @@
+package client;
+
+public class Spot {
+	static int HIGHGROUND = 0;
+	static int GRESS = 1;
+	static int HILL  = 2;
+	static int desert = 3;
+	private Unit assigned_unit;
+	private int terrain;
+
+}
