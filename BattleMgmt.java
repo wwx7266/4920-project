8b5585a6c560a575c05bf6d2bf6469259fc6a5bd
Xanders94 <z3461230@unsw.edu.au>
2016-10-19 22:51:35 +1100

    Update BattleMgmt.java
    continued to implement queuing and battle functionality, still have to test

--- BattleMgmt.java
+++ BattleMgmt.java
@@ -1,68 +1,118 @@
 package Server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 //holds a list of ongoing battle objects
 //determines from player level who should be matched with who
 //holds a list of users currently in matchmaking
-public class BattleMgmt {
+public class BattleMgmt implements Runnable{
 	ArrayList<Battle> battleList = new ArrayList<Battle>();
 	Queue matchQueue = null;
-	BattleMgmt(){
-		
-	}
-	//adds a player to the battle queue
-	//
-	public void addToQueue(String login, String armyName){
-		
-	}
-	//removes user from battle queue
-	public void removeFromQueue(String login){
-		
-	}
-	//determines which battle the player is involved with and makes moves based on input
-	public void makeMove(String login, String moves){
-		
-	}
-	//returns false if the player isn't involved in a battle in the battle list
-	//returns the current state of the board if the user is in a battle
-	public String isInBattle(){
-		
-	}
-	
-}
-//handles the match making queue server side
-class Queue implements Runnable{
-	ArrayList<String[]> queue = new ArrayList<String[]>();
-	BattleMgmt instance = null;
-	Queue(BattleMgmt instance){
-		this.instance = instance;
+	Account accounts = null;
+	Market market = null;
+	BattleMgmt(Account accounts, Market market){
+		this.accounts = accounts;
+		this.market = market;
+		matchQueue = new Queue(this);
 	}
 	public void run(){
 		//matchmaking goes in here
 		//moves users picked into a battle object instance
+	}	
+	public Queue returnQueue(){
+		return this.matchQueue;
 	}
-	public void addToQueue(String login, String armyName){
+}
+//handles the match making queue server side
+class Queue{
+	ArrayList<Player> queue = new ArrayList<Player>();
+	ArrayList<Battle> battleList = new ArrayList<Battle>();
+	BattleMgmt instance = null;
+	Queue(BattleMgmt instance){
+		this.instance = instance;
+	}
+	//adds a player to the battleQueue
+	public boolean addToQueue(String login, String armyName){
+		Player toAdd = null;
+		try{
+			toAdd = new Player(login, armyName, instance);
+		} catch(Exception e){
+			return false;
+		}
+		this.queue.add(toAdd);
+		return true;
 		
 	}
+	//removes a player from the queue given their login
 	public void removeFromQueue(String login){
-		
+		Player toRemove = null;
+		for(Player player: queue){
+			if(player.getName().equals(login)){
+				toRemove = player;
+			}
+		}
+		queue.remove(toRemove);
 	}
 }
 
 class Battle {
-	ArrayList<Player> player1;
-	ArrayList<Player> player2;
-	ArrayList<Unit> army1;
-	ArrayList<Unit> army2;
+	Player player1 = null;
+	Player player2 = null;
+	ArrayList<Unit> army1 = null;
+	ArrayList<Unit> army2 = null;
+	ArrayList<String> commandSet1 = new ArrayList<String>();
+	ArrayList<String> commandSet2 = new ArrayList<String>();
+	String state = null;
+	
+	BattleMgmt instance = null;
+	public Battle(Player player1, Player player2, BattleMgmt instance) throws Exception{
+		this.instance = instance;
+		
+		this.player1 = player1;
+		this.player2 = player2;
+		
+		this.army1 = getUnits(player1);
+		this.army2 = getUnits(player2);
+	}
+	
+	//retrieves the units in a player's army, if the army dosen't exist, returns null
+	public ArrayList<Unit> getUnits(Player player) throws Exception{
+		ArrayList<Unit> output = new ArrayList<Unit>();
+		ArrayList<String[]> units = instance.accounts.displayArmyBattle(player.getName(), player.getArmyName());
+		if(units.isEmpty()){
+			return null;
+		}
+		for(String[] unit : units){
+			output.add(new Unit(Integer.parseInt(unit[0]), unit[2], Integer.parseInt(unit[5]),Integer.parseInt(unit[4]),Integer.parseInt(unit[5]),Integer.parseInt(unit[6])));
+		}
+		return output;
+	}
 	
 }
-
+//represents a player's information in battle and is also used in matchmaking
 class Player {
 	String login;
 	int level;
-	String ArmyName;
-	
+	String armyName;
+	Player(String login, String armyName, BattleMgmt instance) throws Exception{
+		this.login = login;
+		this.level = instance.accounts.getLevel(login);
+		this.armyName = armyName;
+	}
+	public String getName(){
+		return login;
+	}
+	public int getLevel(){
+		return level;
+	}
+	public String getArmyName(){
+		return armyName;
+	}
 }
 class Unit {
 	static int WORRIOR_TYPE = 0;
@@ -79,18 +129,23 @@ class Unit {
 	private int Defence;
 	private int Lv;
 	private int Exp;
+	private int ID;
 	
-	public Unit(String type, int maxhp, int attack, int defence, int lv){
-		Type = type;
-		Name = type;
-		Max_HP = maxhp;
-		Curr_HP = maxhp;
-		Attack = attack;
-		Defence = defence;
-		BP = 0;  //need to implement how to calculate the battle points	
-		Lv = lv;
-		Exp = 0;
+	public Unit(int unitID, String type, int maxhp, int attack, int defence, int lv){
+		this.ID = unitID;
+		this.Type = type;
+		this.Name = type;
+		this.Max_HP = maxhp;
+		this.Curr_HP = maxhp;
+		this.Attack = attack;
+		this.Defence = defence;
+		this.BP = 0;  //need to implement how to calculate the battle points	
+		this.Lv = lv;
+		this.Exp = 0;
 		
+	}
+	public int getID(){
+		return this.ID;
 	}
 	
 	public int fight(Unit enemy){
