4997159ec448d618469b243f739bab180b82d394
Xanders94 <z3461230@unsw.edu.au>
2016-10-24 01:42:43 +1100

    updating to final work submitted

--- ClientConnection.java
+++ ClientConnection.java
@@ -1,9 +1,12 @@
+package Client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class ClientConnection {
@@ -15,142 +18,162 @@ public class ClientConnection {
 	String command = null;
 	String[] result = null;
 	boolean connect = false;
-	Connection connection = null;
+	Connection connection;
 	
-	//ClientConstructor that may take the user name, password, server address and server port
 	ClientConnection(String user, String password, String serverAddress, int serverPort){
-		//Initialization of user credentials
 		this.user = user;
 		this.password = password;
-		//Establishment of server connection
 		this.serverAddress = serverAddress;
 		this.serverPort = serverPort;
-		this.connection = new Connection(this.serverAddress, this.serverPort);
+		this.connection = new Connection(serverAddress,serverPort);
 	}
-	
-	//ClientConnection constructor using default port 2500
-	ClientConnection(String user, String password, String serverAddress){
-		//Initialization of user credentials
-		this.user = user;
-		this.password = password;
-		//Establishment of server connection
-		this.serverAddress = serverAddress;
-		this.connection = new Connection(this.serverAddress, this.serverPort);
+	public void connect(){
+		this.connect = true;
 	}
-	/*commands are: register, login, battleReq, battleMove, infoReq, logout
-	 * 
-	 * sub commands
-	 * battleReq##<battleMode>##<army id>
-	 * battleMove##<action1>##<action2>## etc.
-	 *
-	 * response in form
-	 * register: SvrRes##<username>##true
-	 * login: SvrRes##<username>##<true/false>
-	 * battleReq: SvrRes##<username>##<battleMode>##<opponent>
-	 * battleMove: SvrRes##<username>##battle##<turnNumber>##<result1>##<result2>## etc.
-	 * infoReq: SvrRes##<username>##army:<number>##<unit1info>##<unit2info> etc.
-	 * 
-	 */
-
-	
+	public void disconnect(){
+		this.connect = false;
+	}
 	//registers a user with the initialized information
-	public String[] register() throws IOException{
+	public boolean register() throws IOException{
 		this.command = "register";
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
+		String payload = connection.sendRecieve("UsrReq##"+user+"##"+password+"##"+command);
+		String[] result = payload.split("##");
+		if(result[2].equals("true")){
+			return true;
+		} else {
+			return false;
+		}
 	}
 	//logs in user with presented parameters
-	public String[] login() throws IOException{
-		this.command = "login";
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
+	public boolean login() throws IOException{
+		String command = "login";
+		String payload = connection.sendRecieve("UsrReq##"+user+"##"+password+"##"+command);
+		String[] result = payload.split("##");
+		if(!result[2].equals("Couldn't Login")){
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+	public ArrayList<Unit> getUnits() throws IOException{
+		this.command = "infoReq";
+		String payload = connection.sendRecieve("UsrReq##"+user+"##"+password+"##"+command);
+		String[] result = payload.split("##");
+		ArrayList<String> rawUnits = new ArrayList<String>(Arrays.asList(result));
+		return convertStringToUnit(rawUnits);
+	}
+	private ArrayList<Unit> convertStringToUnit(ArrayList<String> rawUnits){
+		ArrayList<Unit> output = new ArrayList<Unit>();
+		int count = 0;
+		for(String rawUnit : rawUnits){
+			if(count < 2){
+				count++;
+				continue;
+			}
+			String[] params = rawUnit.split("#-#");
+			//type, hp, attack, defense
+			output.add(new Unit(params[1],Integer.parseInt(params[0]), typeToCode(params[2]), Integer.parseInt(params[5]), Integer.parseInt(params[4]), Integer.parseInt(params[6]), user));
+		}
+		return output;
+	}
+	private int typeToCode(String type) {
+		if(type.equals("Swordsman")){
+			return 0;
+		} else if(type.equals("Pikeman")){
+			return 1;
+		} else if(type.equals("Knight")){
+			return 2;
+		} else if(type.equals("Archer")){
+			return 3;
+		}
+		return -1;
+	}
 	//sends a battle request to the server, 
-	//will receive a reply request with non null opponent if successful
-	public String[] RequestBattle(String battleMode, int armyID) throws IOException{
-		/*
-		 * command in form:
-		 * battleReq##<battleMode>##<army id>
-		 */
-		this.command = "battleReq##"+battleMode+"##"+armyID;
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
+	//will receive a reply mirroring request if successful
+	public ArrayList<MatchUnit> RequestBattle(String army) throws IOException{
+		ArrayList<MatchUnit> resultList = new ArrayList<MatchUnit>();
+		
+		String command = "battleReq##"+army;
+		String payload = connection.sendRecieve("UsrReq##"+user+"##"+password+"##"+command);
+		String[] result = payload.split("##");
+		while(result[3].equals("wait")){
+			payload = connection.sendRecieve("UsrReq##"+user+"##"+password+"##"+command);
+			result = payload.split("##");
+		}
+		ArrayList<String> input = new ArrayList<String>(Arrays.asList(result));
+		int count = 0;
+		for(String line : input){
+			if(count < 3){
+				//skip 3 times
+				count++;
+				continue;
+			}
+			if(line.length() < 4){
+				continue;
+			}
+			String[] split = line.split("#-#");
+			resultList.add(new MatchUnit(split[1], Integer.parseInt(split[0]), split[2]));
+		}
+		return resultList;
 	}
 	//sends the move made by user in battle, any number of moves may be tacked onto
 	//the end of the message
-	public String[] BattleAction(ArrayList<String> battleActions) throws IOException{
-		/*
-		 * command in form:
-		 * battleMove##<action1>##<action2>## etc.
-		 * 
-		 * response in form:
-		 * SvrRes##<username>##battle##<turnNumber>##<result1>##<result2>## etc.
-		 */
-		this.command = "battleMove##";
-		for(String action : battleActions){
-			this.command = this.command + action + "##";
+	public String[] BattleAction(int UnitID) throws IOException{
+		String command = "battleMove##"+ UnitID;
+		String payload = connection.sendRecieve("UsrReq##"+user+"##"+password+"##"+command);
+		String[] result = payload.split("##");
+		while(result[3].equals("wait")){
+			payload = connection.sendRecieve("UsrReq##"+user+"##"+password+"##battleMove##-1");
+			result = payload.split("##");
+			System.out.println(result[2]);
 		}
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
+		return result;
 	}
 	//used to request information from the server on current armies and their composition
-	public String[] requestInfo() throws IOException{
-		/*
-		 * server response in form:
-		 * SvrRes##<username>##army:<number>##<unit1info>##<unit2info> etc.
-		 */
+	public String[] requestInfo(){
 		this.command = "infoReq";
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
 		return this.result;
 	}
-	//used to request current information on the economy
-	public String[] requestInfoEcon() throws IOException{
-		this.command = "infoReqEcon";
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
+	//
+}
+class ClientSide implements Runnable{
+	ClientConnection info = null;
+	public ClientSide(ClientConnection clientConnection) {
+		this.info = clientConnection;
 	}
-	//used to purchase units from the store
-	public String[] purchaseEcon(int unitID) throws IOException{
-		/*
-		 * command in form:
-		 * purchase##<unitID>
-		 * 
-		 * Response:
-		 * purchase##<true/false>
-		 */
-		this.command = "purchase##" + unitID;
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
-	}
-	//used to send a message via chat
-	public String[] chatUp(String payload) throws IOException{
-		this.command = "chatUp##" + payload;
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
-	}
-	//used to add a friend to account
-	public String[] addFriend(String friend) throws IOException{
-		this.command = "friendOp##add##" + friend;
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
-	}
-	//used to remove a friend from the account
-	public String[] removeFriend(String friend) throws IOException{
-		this.command = "friendOp##delete##" + friend;
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
-	}
-	//used to retrieve current friends list from server
-	public String[] displayFriend() throws IOException{
-		this.command = "friendOp##display";
-		this.result = connection.sendRecieve("UsrReq##"+this.user+"##"+this.password+"##"+this.command).split("##");
-		return this.result;
+
+	public void run(){
+		
+		String payload = null;
+		String commandLine = null;
+		Connection connection = new Connection(info.serverAddress,info.serverPort);
+		while(true){
+			if(info.connect){
+				try {
+					info.disconnect();
+					//commands are: register, login, battleReq, battleMove, infoReq, logout
+					/* sub commands
+					 * battleReq##<battleMode>##<army id>
+					 * battleMove##<unitID>
+					 */
+					payload = connection.sendRecieve("UsrReq##"+info.user+"##"+info.password+"##"+info.command);
+					/*payload in form
+					 * register: SvrRes##<username>##true
+					 * login: SvrRes##<username>##true
+					 * battleReq: SvrRes##<username>##<battleMode>##<army id>
+					 * battleMove: SvrRes##wait
+					 * 				svrRes##message##<message1>##<message2>## etc.
+					 * 				svrRes##turnEnd##<message1>##<message2>## etc.
+					 * 					 * infoReq: SvrRes##<username>##army:<number>##<unit1info>##<unit2info> etc.
+					*/
+					info.result = payload.split("##");
+				} catch (IOException e) {
+					System.out.println("Connection failed, trying to reestablish");
+				}
+			}
+		}
 	}
 }
-/*
- * this class can, given a server and socket, send and receive data from a server
- */
 class Connection{
 	public String serverName = null;
 	public int portNumber = 25000;
@@ -164,8 +187,7 @@ class Connection{
 		this.portNumber = portNumber;
 		
 	}
-	
-	//will send a given string to a server and receive the response and convert back into ASCII characters
+
 	public String sendRecieve(String payload) throws IOException{
 		String serverPayload = null;
 		Socket serverConnection = null;
@@ -188,4 +210,15 @@ class Connection{
 		
 		return serverPayload;
 	}
+	public void pullEst() throws UnknownHostException, IOException{
+		serverConnection = new Socket (this.serverName,this.portNumber);
+		upload = new PrintWriter(serverConnection.getOutputStream(), true);
+		download = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
+	}
+	public void pullStop() throws IOException{
+		upload.close();
+		download.close();
+		serverConnection.close();
+	}
+	
 }
